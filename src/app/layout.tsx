c604e06b232b69f584e261b9e3d6921e8517e522
Kam0-M <kamohelo.thakhisi@gmail.com>
2026-05-07 08:49:37 -0400

    Clarity - Function

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { ToastProvider } from "@/components/providers/toast-provider";
-import Script from 'next/script';
+import ClarityProvider from '@/components/providers/clarity-provider'
 
 export const metadata: Metadata = {
   title: 'Invonaut - From Contract to Cash. Automated.',
@@ -22,21 +22,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      
-      {/* ✅ Clarity script goes here */}
-      <Script
-        id="microsoft-clarity"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `(function(c,l,a,r,i,t,y){
-            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-          })(window, document, "clarity", "script", "wn0k9cxu7r");`,
-        }}
-      />
 
       <body className="font-sans antialiased">
+        <ClarityProvider />
         {children}
         <ToastProvider />
       </body>
